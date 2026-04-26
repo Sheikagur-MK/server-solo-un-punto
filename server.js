d0a137ec02d7e7580a796883f432ade2903f4977
Sheikagur-MK <hugok2002@gmail.com>
2026-04-26 11:48:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const io = new Server(server, {
     cors: {
         origin: "*", 
         methods: ["GET", "POST"]
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
     }
 });
 
