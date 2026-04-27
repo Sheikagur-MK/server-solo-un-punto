a84b99cdcec298ef444f6d4cbc0b7f0cdccbe698
Sheikagur-MK <hugok2002@gmail.com>
2026-04-26 19:42:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+server.listen(PORT,'0.0.0.0', () => {
     console.log(`🚀 Servidor funcionando en puerto ${PORT}`);
 });
