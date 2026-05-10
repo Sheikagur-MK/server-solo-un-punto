0c62ca6da8c440111233ff4c8127dfbea164740d
Sheikagur-MK <hugok2002@gmail.com>
2026-05-10 15:43:38 -0700

    Update server.js

--- mi-juego-geo/server.js
+++ mi-juego-geo/server.js
@@ -1,1 +1,43 @@
 
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let players = {};
+
+io.on('connection', (socket) => {
+    console.log('Jugador conectado:', socket.id);
+    
+    players[socket.id] = {
+        x: Math.random() * 800,
+        y: Math.random() * 600,
+        color: `hsl(${Math.random() * 360}, 70%, 50%)`,
+        shape: 'circle',
+        score: 0
+    };
+
+    io.emit('updatePlayers', players);
+
+    socket.on('move', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            socket.broadcast.emit('playerMoved', { id: socket.id, x: data.x, y: data.y });
+        }
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        io.emit('updatePlayers', players);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
